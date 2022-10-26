3a043a827bd89c694e2bb32446e68642b47b1169
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-10-26 15:26:39 -0700

    Product Tag model associations

--- models/index.js
+++ models/index.js
@@ -8,19 +8,21 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { 
-  through: {
-    model: ProductTag
-  }});
+ProductTag.belongsToMany(Product, { 
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { 
-  through: {
-    model: ProductTag
-  }});
+Product.belongsToMany(ProductTag, { 
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
